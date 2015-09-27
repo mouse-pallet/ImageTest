625e361cb92ce1d3b3af14a55798caa33aee3ad1
mouse-pallet <tea_erra@yahoo.co.jp>
2015-09-27 17:36:45 +0900

    move to touch point

--- ImageTest/ViewController.m
+++ ImageTest/ViewController.m
@@ -15,8 +15,45 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // 画像の生成
+    UIImage *image = [UIImage imageNamed:@"image01.png"];
+    //UIImageViewに画像を乗せる
+    _iv = [[UIImageView alloc] initWithImage:image];
+    //分かりやすく背景追加
+    _iv.backgroundColor = [UIColor whiteColor];
+    //UIImageViewの位置とサイズを指定
+    [_iv setFrame:CGRectMake(50.0, 50.0, image.size.width, image.size.height)];
+    //ビューに表示
+    [self.view addSubview:_iv];
+    
+    /********
+    // 画像を表示する
+    UIImage *image = [UIImage imageNamed:@"image01.png"];
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+    imageView.center = CGPointMake(160, 240);
+    [self.view addSubview:imageView];
+    
+    // ビューをx方向に10px，y方向に50px移動
+    CGAffineTransform t1 = CGAffineTransformMakeTranslation(10, 50);
+    
+    // ビューを45度回転
+    float angle = 45.0 * M_PI / 180;
+    CGAffineTransform t2 = CGAffineTransformRotate(t1, angle);
+    
+    imageView.transform = t2;
+     **********/
+}
+
+
+//タッチされたときに呼ばれるメソッド
+- (void) touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event
+{
+    //タッチイベントを取り出す
+    UITouch *touch = [touches anyObject];
+    //タッチイベントから座標を取得
+    CGPoint point = [touch locationInView:self.view];
+    //画像(UIImageView)の中心座標とタッチイベントから取得した座標を同期
+    _iv.center = point;
 }
 
 - (void)didReceiveMemoryWarning {
